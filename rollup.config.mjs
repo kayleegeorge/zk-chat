8de6c9f447e1d49aaf8a15b19958d52bd6bbae2b
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-02-10 07:31:10 +0000

    Rollup and ESLint

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+
+import typescript from 'rollup-plugin-typescript2';
+import cleaner from 'rollup-plugin-cleaner';
+import { visualizer } from "rollup-plugin-visualizer";
+import * as fs from "fs"
+
+const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"))
+const banner = `/**
+ * @module ${pkg.name}
+ * @version ${pkg.version}
+ * @file ${pkg.description}
+ * @copyright Ethereum Foundation 2022
+ * @license ${pkg.license}
+ * @see [Github]{@link ${pkg.homepage}}
+*/`
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: pkg.exports.require, format: "cjs", banner, exports: "auto" },
+    { file: pkg.exports.import, format: "es", banner }
+  ],
+  plugins: [
+    cleaner({
+      targets: [
+        './dist/'
+      ]
+    }),
+    typescript({tsconfig: 'tsconfig.build.json'}),
+    visualizer({
+      emitFile: true,
+      filename: "stats.html",
+      template: "sunburst"
+    }),
+  ]
+};
