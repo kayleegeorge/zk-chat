a39422933d0991f0f422932bcaa5836365ff74c4
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2023-02-13 10:01:27 -0800

    uncomment

--- tests/chatApp.test.ts
+++ tests/chatApp.test.ts
@@ -1,9 +1,9 @@
-// import { ChatApp, ChatRoom } from '../dist'
+import { ChatApp, ChatRoom } from '../dist'
 import { ChatMessage } from '../dist/proto/ChatMessage'
-// import { RoomType } from '../dist/types/ChatRoomOptions'
+import { RoomType } from '../dist/types/ChatRoomOptions'
 import { utf8ToBytes } from '../dist/utils/formatting'
 import { ChatMessage as ChatMsg } from '../src/types/ChatMessage'
-// import { createWakuNode } from '../src/utils/createWakuNode'
+import { createWakuNode } from '../src/utils/createWakuNode'
 
 // to do: delete either chatMessage or chatmsg
 describe('test protons', () => {
@@ -31,40 +31,40 @@ describe('test types', () => {
   expect(decodedMsg).toBeInstanceOf(ChatMsg)
 })
 
-// describe('waku node', async () => {
-//   it('creates waku note without error', async function () {
-//     await createWakuNode()
-//   })
-// })
+describe('waku node', async () => {
+  it('creates waku note without error', async function () {
+    await createWakuNode()
+  })
+})
 
-// describe('chatApp', () => {
-//   const app = new ChatApp('test-app', false)
-//   //const rlnMem = app.rln.rlnjs.identity
-//   it('Expect rln identifier to be big int', () => {
-//     const rlnIdentifier = app.rln.rlnIdentifier
-//     expect(rlnIdentifier).toBe(BigInt)
-//   })
+describe('chatApp', () => {
+  const app = new ChatApp('test-app', false)
+  //const rlnMem = app.rln.rlnjs.identity
+  it('Expect rln identifier to be big int', () => {
+    const rlnIdentifier = app.rln.rlnIdentifier
+    expect(rlnIdentifier).toBe(BigInt)
+  })
   
-//   it('expect rln member', () => {
-//     const rlnMember = app.rln.identityCommitment
-//     expect(rlnMember).toBe(BigInt)
-//   })
+  it('expect rln member', () => {
+    const rlnMember = app.rln.identityCommitment
+    expect(rlnMember).toBe(BigInt)
+  })
 
-//   const chatroom = new ChatRoom('test-chatroom', RoomType.PubGroup, [], app.rln, app.connection)
+  const chatroom = new ChatRoom('test-chatroom', RoomType.PubGroup, [], app.rln, app.connection)
 
-//   it('create chat room', () => {
-//     expect(chatroom).toBe(ChatRoom)
-//     const chatMembers = chatroom.getChatMembers()
-//     expect(chatMembers[0]).toBe(String)
-//   })
+  it('create chat room', () => {
+    expect(chatroom).toBe(ChatRoom)
+    const chatMembers = chatroom.getChatMembers()
+    expect(chatMembers[0]).toBe(String)
+  })
 
-//   it('send message', async () => {
-//     await chatroom.sendMessage('test msg', 'klee')
-//   })
+  it('send message', async () => {
+    await chatroom.sendMessage('test msg', 'klee')
+  })
 
-//   it('process message', async () => {
-//     const msgsSent = await chatroom.retrieveMessageStore()
-//     expect(msgsSent[0]).toBe(ChatMessage)
-//   })
+  it('process message', async () => {
+    const msgsSent = await chatroom.retrieveMessageStore()
+    expect(msgsSent[0]).toBe(ChatMessage)
+  })
   
-// })
+})
